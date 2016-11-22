4ed82d1f083f2fd60cee2a7cacb9de08f4114f25
Ajay Negi <ajaynegi0070@gmail.com>
2016-11-22 16:10:51 +0530

    Update scroll-header.js

--- scroll-header.js
+++ scroll-header.js
@@ -1,3 +1,18 @@
+
+//primary
+var previousScroll = 0;
+   $(window).scroll(function() {
+       var currentScroll = $(this).scrollTop();
+       if (currentScroll > previousScroll) $('body:not(.page-template-page-customer-onboarding) .header-container').removeClass('animate');
+       else if (currentScroll < 1) $('body:not(.page-template-page-customer-onboarding) .header-container').removeClass('animate');
+       else $('body:not(.page-template-page-customer-onboarding) .header-container').addClass('animate');
+       previousScroll = currentScroll;
+   });
+
+
+
+
+//secondary
 $(window).scroll(function(){
     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
     shrinkOn = 300,
